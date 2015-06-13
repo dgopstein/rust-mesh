584c4a8d87ab1d935661215e4c21d5e4c7bb57d7
Dan Gopstein <dan@gopstein.com>
2015-06-13 17:07:51 -0400

    Parse assimp file

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,21 @@
+extern crate assimp;
+
+use std::env;
+
 fn main() {
-    println!("Hello, world!");
+  let print_usage_and_die = || {
+      println!("Usage: rust-mesh filename");
+      std::process::exit(1)
+    };
+
+  let filename = &env::args().nth(1).unwrap_or_else(print_usage_and_die);
+
+  println!("Loading file: {}", filename);
+
+  let aiScene  = assimp::load(filename, 0).unwrap_or_else(|str| {
+    println!("An error occured parsing the scene: {}", str);
+    std::process::exit(2)
+  });
+
+  println!("aiScene.mNumMeshes: {}", aiScene.mNumMeshes);
 }
