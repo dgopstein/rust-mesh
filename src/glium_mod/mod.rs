d99a05837e8c64dd7e238874bfdcf2245c0890c4
Dan Gopstein <dan@gopstein.com>
2015-06-20 14:30:55 -0400

    Reformat to 4-space indent

--- src/glium_mod/mod.rs
+++ src/glium_mod/mod.rs
@@ -36,51 +36,46 @@ fn octahedron(length: f32) -> Vec<Vertex> {
 }
 
 #[derive(Copy, Clone)]
- struct Vertex {
-   position: [f32; 2],
- }
+struct Vertex {
+    position: [f32; 2],
+}
 
 #[cfg(feature = "window")]
 pub fn open_window() {
-  use glium;
-  use glium::{DisplayBuild, Surface};
-  let display = glium::glutin::WindowBuilder::new().build_glium().unwrap();
+    use glium;
+    use glium::{DisplayBuild, Surface};
+    let display = glium::glutin::WindowBuilder::new().build_glium().unwrap();
 
-  implement_vertex!(Vertex, position);
+    implement_vertex!(Vertex, position);
 
-  let shape = octahedron(0.5);
+    let shape = octahedron(0.5);
 
-  let vertex_buffer = glium::VertexBuffer::new(&display, shape);
-  let indices = glium::index::NoIndices(glium::index::PrimitiveType::TrianglesList);
+    let vertex_buffer = glium::VertexBuffer::new(&display, shape);
+    let indices = glium::index::NoIndices(glium::index::PrimitiveType::TrianglesList);
 
-  let vertex_shader_src = r#"
-    #version 140
+    let vertex_shader_src = r#"
+        #version 140
 
-    in vec2 position;
+        in vec2 position;
 
-    uniform mat4 matrix;
+        uniform mat4 matrix;
 
-    void main() {
-        gl_Position = matrix * vec4(position, 0.0, 1.0);
-      }
-  "#;
+        void main() {
+            gl_Position = matrix * vec4(position, 0.0, 1.0);
+        }
+    "#;
 
-  let fragment_shader_src = r#"
-    #version 140
+    let fragment_shader_src = r#"
+        #version 140
 
-    out vec4 color;
+        out vec4 color;
 
-    void main() {
-      color = vec4(1.0, 0.0, 0.0, 1.0);
-    }
-  "#;
+        void main() {
+          color = vec4(1.0, 0.0, 0.0, 1.0);
+        }
+    "#;
 
-  let program = glium::Program::from_source(&display, vertex_shader_src, fragment_shader_src, None).unwrap();
-
-  let mut t = -0.5;
-
-  //loop {
-    // we update `t`
+    let program = glium::Program::from_source(&display, vertex_shader_src, fragment_shader_src, None).unwrap();
 
     let draw_scene = |x: f32, y: f32| {
         let mut target = display.draw();
@@ -102,15 +97,15 @@ pub fn open_window() {
 
     for event in display.wait_events() {
         match event {
-          glium::glutin::Event::Closed => {
-              println!("Closing: {:?}", event);
-              break;
-          }
-          glium::glutin::Event::MouseMoved((x, y)) => {
-              println!("Mouse: ({}, {})", x, y);
-              draw_scene((x as f32)/800.0, (y as f32)/600.0);
-          }
-          event => println!("Event: {:?}", event)
+            glium::glutin::Event::Closed => {
+                println!("Closing: {:?}", event);
+                break;
+            }
+            glium::glutin::Event::MouseMoved((x, y)) => {
+                println!("Mouse: ({}, {})", x, y);
+                draw_scene((x as f32)/800.0, (y as f32)/600.0);
+            }
+            event => println!("Event: {:?}", event)
         }
     }
 }
