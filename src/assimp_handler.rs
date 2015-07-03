fb5e55f280fcdb7e5246de2435ed0809a8992541
Dan Gopstein <dan@gopstein.com>
2015-07-03 10:27:35 -0400

    map_nodes without closure-ref and with using

--- src/assimp_handler.rs
+++ src/assimp_handler.rs
@@ -5,47 +5,26 @@ pub struct Bone<'a> {
     childen: Vec<&'a Bone<'a>>
 }
 
-pub fn map_nodes<R>(f: &Fn(&aiNode) -> R, n: &aiNode) -> Vec<R>
-    // where F : Fn(&aiNode) -> R {
-    {
-
-    let mut v: Vec<R> = Vec::new();
-    let mapped: R = f(n);
-    v.push(mapped);
-
-    let children = n.children();
-
-    let mapped_children =
-        children.iter().flat_map( |child|
-            map_nodes(f, &child)
-        );
-
-    v.extend( mapped_children );
-
-    v
+fn using<X, F, R>(mut x: X, f: F) -> X
+        where F: Fn(&mut X) -> R {
+    f(&mut x);
+    x
 }
 
-// use core;
-// pub fn map_nodes<F, R>(f: F, n: Box<aiNode>) -> Vec<R>
-//     where F : FnMut(Box<aiNode>) -> R {
-//
-//     let mut all_nodes = vec![n];
-//     {
-//         let mut borrowed = &mut all_nodes;
-//
-//         for &mut node in borrowed {
-//             borrowed.extend(node.children());
-//         }
-//     }
-//
-//     let all_mapped: Vec<R> = all_nodes.map_in_place(f);
-//
-//     all_mapped
-// }
+pub fn map_nodes<F, R>(f: F, n: &aiNode) -> Vec<R>
+        where F : Fn(&aiNode) -> R {
+    map_nodes_rec(&f, n)
+}
+
+fn map_nodes_rec<R>(f: &Fn(&aiNode) -> R, n: &aiNode) -> Vec<R> {
+    using(vec![f(n)], |v| {
+        v.extend(n.children().iter().flat_map(|child| map_nodes(f, &child)));
+    })
+}
 
 pub fn parse_scene(scene: &aiScene) -> Option<Bone> {
     println!("Root node name: {}", scene.root_node().name());
-    println!("All names: {:?}", map_nodes(&|x|x.name(), &scene.root_node()));
+    println!("All node names: {:?}", map_nodes(|x|x.name(), &scene.root_node()));
 
     None
 }
